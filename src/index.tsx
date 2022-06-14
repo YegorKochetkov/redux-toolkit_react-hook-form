61cf560ceb8fe1f7b03612f9cf213eee2bcf5e0a
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-14 18:02:14 +0300

    Add components to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { FormModal } from "./features/Form/Form";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="invoiceAdressModal" element={<FormModal />} />
+            <Route path="bankDataModal" element={<FormModal />} />
+            <Route path="contactModal" element={<FormModal />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
